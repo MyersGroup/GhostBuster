a17f5a0255c56ff3b7b085e0647212bcb03523a3
Leo Speidel <speidel@login001.camp.thecrick.org>
2021-10-12 18:17:36 +0100

    Added Rscripts and backup of EM script

---
+++ Rscripts/analyse_coancestry.R
@@ -0,0 +1,207 @@
+library(relater)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(purrr)
+#library(nls2)
+library(stats)
+library(reticulate)
+library(parallel)
+np <- import("numpy")
+
+set.seed(1)
+
+binsize <- 0.05
+binmax  <- 10
+recbins <- as.numeric(seq(0,binmax,binsize))
+L       <- length(recbins)
+
+filename_local_ancestry <- "True_ground_truth_membership_0.npy"	
+filename_mut <- "msprime"
+
+if(1){
+
+  member <- np$load(filename_local_ancestry)
+  member   <- member[-1,]
+  gwmember <- rowSums(member)
+  gwmember <- gwmember/sum(gwmember)
+  member <- member/gwmember
+
+  chrs <- c(1)
+
+  #Calculate genetic distance between adjacent trees.
+  df <- data.frame()
+  for(chr in chrs){
+
+    mut    <- as.data.frame(read.mut(paste0(filename_mut, "_chr",chr,".mut.gz")))
+    #mut    <- as.data.frame(read.mut(paste0("./relate_homsap_chr",chr,".mut.gz")))
+    index  <- c(1, which(diff(mut$tree_index) > 0))
+    if(max(index)+1 == nrow(mut)){
+      index  <- unique(c(index, nrow(mut)))
+      bp_raw <- c(as.numeric(mut[index,"pos_of_snp"]), max(mut$pos_of_snp)+1)
+    }else{
+      index  <- unique(c(index, nrow(mut)))
+      bp_raw <- as.numeric(mut[index,"pos_of_snp"])
+    }
+    bp     <- (bp_raw[-length(bp_raw)] + bp_raw[-1])/2
+
+    rec <- read.table(paste0("/camp/lab/skoglundp/working/leo/datasets/human_genome/recomb_maps/genetic_map_combined_b37_chr",chr,".txt"), header = T)
+    rec <- stepfun(rec[-1,1], rec[,3])
+
+    recrates_start <- rec(bp_raw[-length(bp_raw)])
+    recrates_end   <- rec(bp_raw[-1])
+    df <- rbind(df, cbind(CHR = chr, BP = bp, BP_start = bp_raw[-length(bp_raw)], BP_end = bp_raw[-1], rec = as.numeric(recrates_end - recrates_start), recdist_start = as.numeric(recrates_start), recdist_end = as.numeric(recrates_end)))
+
+  }
+
+  print(dim(member))
+  df <- cbind(df, t(member))
+  df %>% group_by(CHR) %>% filter(rec > 0, rec < 0.5) -> df
+  df %>% group_by(CHR) -> df
+  df$BP_bin <- cut(df$BP, breaks = seq(0,300e6,10e6))
+
+  #read in recombination rates
+  #for each tree in the genome, choose midpoint and store as BP
+  #populate matrix of form "number of trees x cM away" with coancestry curve, i.e. pi*qj/(p * q)
+
+  df_coan <- data.frame()
+
+  for(comp1 in c("1", "2", "3", "4")){
+    for(comp2 in c("1", "2", "3", "4")){
+
+      df %>% group_by(CHR) %>% mutate(ind = 1:length(BP)) -> df_all
+      df_all %>% group_by(CHR, BP_bin) %>% summarize( ind = list(ind[sample(1:length(BP),min(length(BP),30), replace = F)]) ) %>% unnest(cols = ind) -> df_chosen
+
+      for(chr in chrs){
+
+        df <- subset(df_all, CHR == chr)
+
+        df[,comp1] <- as.numeric(as.matrix(df[,comp1]))
+        df[,comp2] <- as.numeric(as.matrix(df[,comp2]))
+
+        get_coan_for_tree <- function(i){
+
+          df_tmp <- df
+          p <- as.numeric(df_tmp[i,comp1])
+
+          if(p > 0){
+            #r <- as.numeric((df_tmp[i,"recdist_end"] + df_tmp[i,"recdist_start"])/2)
+            r <- runif(n = 1, min = as.numeric(df_tmp[i,"recdist_start"]), max = as.numeric(df_tmp[i,"recdist_end"]))
+            df_tmp[,"recdist_end"]   <- as.matrix(round(abs(df_tmp[,"recdist_end"] - r)/binsize) + 1)
+            df_tmp[,"recdist_start"] <- as.matrix(round(abs(df_tmp[,"recdist_start"] - r)/binsize) + 1)
+            df_tmp$recdist_end[df_tmp$recdist_end > L] <- L
+            df_tmp$recdist_start[df_tmp$recdist_start > L] <- L
+            df_tmp <- df_tmp[-i,]
+
+            df_tmp %>% filter(recdist_start != L | recdist_end != L) -> df_tmp
+            df_tmp$val <- as.numeric(as.matrix(df_tmp[,comp2] * p))
+            df_tmp %>% group_by(CHR,BP) %>% mutate(ind = list(recdist_start:recdist_end)) %>% unnest(cols = ind) %>% group_by(ind) %>% summarize(res = mean(val)) -> df_tmp
+
+            res   <- rep(NA,L)
+            res[df_tmp$ind] <- df_tmp$res
+            return(c(i, res))
+          }else{
+
+            res   <- rep(0,L)
+            return(c(i,res))
+          }
+
+        }
+
+        df_coan_tmp <- cbind(as.data.frame(t(mapply(get_coan_for_tree, subset(df_chosen, CHR == chr)$ind))), comp1 = comp1, comp2 = comp2)
+        df_coan_tmp <- cbind(df_coan_tmp, CHR = chr, BP_bin = df$BP_bin[df_coan_tmp[,1]])
+        df_coan     <- rbind(df_coan, df_coan_tmp)
+      }
+
+    }
+  }
+
+  df_coan %>% pivot_longer(cols = !c(CHR,BP_bin,comp1,comp2, V1), names_to = "dist", values_to = "coancestry") -> df_coan
+  df_coan$dist <- as.matrix(df_coan$dist)
+  foo          <- recbins + binsize/2
+  df_coan      <- merge(df_coan, cbind(dist = paste0("V",1+1:length(foo)), val = foo), by = "dist")
+  df_coan      <- subset(df_coan, !is.na(coancestry))
+
+  save(df, df_coan, file = "coan.RData")
+
+}
+
+
+#fit coancestry curves
+load("coan.RData")
+df_coan$val <- as.numeric(df_coan$val)
+
+
+if(1){
+  num_boot <- 10
+
+  df_params <- data.frame()
+
+  for(c in unique(df_coan$comp1)){ 
+
+    df_sub <- subset(df_coan, comp1 == paste0(c) & comp2 == paste0(c))
+
+    all_blocks <- 1:length(  unique(paste0(df_sub$CHR,"-",df_sub$BP_bin))  )
+    df_sub %>% group_split(CHR, BP_bin) -> df_sub
+
+    for(i in 1:num_boot){
+
+      blocks <- sort(sample(all_blocks,replace = T))
+      bind_rows(df_sub[blocks]) %>% filter() %>% group_by(val) %>% summarize(coancestry = mean(coancestry)) -> df_resampled
+
+      astart <- 3
+
+      goo <- lm( log(df_resampled$coancestry[1:20] - 1) ~ df_resampled$val[1:20]  )
+      astart <- exp(goo$coefficients[1])
+      bstart <- -goo$coefficients[2]*100
+
+      bstart <- 200
+      print(c(astart,bstart))
+
+      #astart <- mean((df_resampled$coancestry-1)/exp(-as.numeric(df_resampled$val)/100*bstart))
+      fitted <- nls(data = df_resampled, algorithm = "port",
+                    coancestry ~ c+a*exp(-as.numeric(val)/100*b), start = list(a = astart, b = bstart, c = 1), control = list(minFactor = 1e-10, maxiter = 1e4), lower = c(0,0))
+      df_params <- rbind(df_params, data.frame(comp1 = c, comp2 = c, boot = i, date = round(28*summary(fitted)$parameters[2,1]), a = summary(fitted)$parameters[1,1], c = summary(fitted)$parameters[3,1]))
+
+    }
+
+  }
+
+
+  df_params %>% group_by(comp1, comp2) %>% summarize(mean = mean(date), lower = quantile(date, p = 0.025), upper = quantile(date, p = 0.975),
+                                                     mean_a = mean(a), lower_a = quantile(a, p = 0.025), upper_a = quantile(a, p = 0.975),
+                                                     mean_c = mean(c), lower_c = quantile(c, p = 0.025), upper_c = quantile(c, p = 0.975),
+                                                     ) -> df_params_sum
+  print(df_params_sum)
+
+
+  df_params %>% group_by(comp1, comp2, boot) %>% mutate( val = list(seq(0,binmax,0.01)), 
+                                                        coancestry = list(c+a*exp(-as.numeric(seq(0,binmax,0.01))/100*date/28))
+                                                        ) %>% unnest(cols = c(val, coancestry)) %>%
+  group_by(comp1, comp2, val) %>% summarize(mean = mean(coancestry), lower = quantile(coancestry, p = 0.025), upper = quantile(coancestry, p = 0.975)) -> df_curves
+
+
+
+  df_curves$val <- as.numeric(df_curves$val)
+  df_curves$mean <- as.numeric(df_curves$mean)
+  df_curves$lower <- as.numeric(df_curves$lower)
+  df_curves$upper <- as.numeric(df_curves$upper)
+
+  print(head(df_curves))
+}
+
+df_coan %>% group_by(comp1, comp2, val) %>% summarize(coancestry = mean(coancestry)) -> df_coan
+
+p <- ggplot(df_coan) +  
+geom_line(data = df_curves, aes(x = val, y = mean)) + 
+geom_ribbon(data = df_curves, aes(x = val, ymin = lower, ymax = upper), alpha = 0.5) + 
+geom_point(aes(x = val, y = coancestry)) + facet_grid(comp1~ comp2) +
+theme_bw()
+
+ggsave(p, file = "plot_coancestry.pdf", width = 10, height = 10)
+
+#Do a block bootstrap
+
+#also look at posterior vs recombination rate, posterior vs quality, posterior correlation to adjacent trees vs recombination rate
+
+
---
+++ Rscripts/plot.R
@@ -0,0 +1,180 @@
+library(relater)
+library(ggplot2)
+library(cowplot)
+library(dplyr)
+library(tidyr)
+library(pammtools)
+library(grid)
+library(ggplotify)
+library(nls2)
+library(ggthemes)
+library(reticulate)
+np <- import("numpy")
+
+palette <- c("#E69F00", "#000000", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+colours <- c("#ff99c8", "#fcf6bd", "#d0f4de", "#a9def9")
+
+colours <- c("#FF5CA8", "#FAF19E", "#BDEFD0", "#8DD4F7", "#CF90F4", "#d0f4de")
+
+palette  <- c("#f2cc8f", "#e07a5f", "#3d405b", "#81b29a")
+
+
+poplabs <- sort(unique(read.table("./data/poplabels.txt", header = T)[,2]))
+
+#poplabels <- read.table("../result/cond_coal_rates_new_group/SGDP_group.poplabels", header = T)[,2]
+#assignments <- unique(poplabels)
+
+read.coal <- function(filename){
+
+	groups <- as.matrix(utils::read.table(filename, nrow = 1))
+	epochs <- as.matrix(utils::read.table(filename, nrow = 1, skip = 1))
+	coal   <- utils::read.table(filename, skip = 2)
+
+	coal[,1] <- as.factor(coal[,1])
+	coal[,2] <- as.factor(coal[,2])
+
+	colnames(coal)[-c(1:2)] <- epochs
+	colnames(coal)[1:2]     <- c("group1", "group2")
+	coal                    <- reshape2::melt(coal, id.vars = c("group1", "group2"))
+	colnames(coal)[3:4]     <- c("epoch.start", "haploid.coalescence.rate")
+	coal                    <- coal[order(paste(coal[,1], coal[,2], sep = "")),]
+	coal$epoch.start        <- as.numeric(as.matrix(coal$epoch.start))
+
+	coal$group2 <- poplabs[as.numeric(as.matrix(coal$group2))+1]
+
+	#coal <- subset(coal, group2 %in% assignment[grep(as.matrix(assignment), pattern = "v1")] )
+	#coal <- subset(coal, group2 %in% c("Sardinian", "Khomani-San", "Mbuti", "Mende"))
+
+	return(coal)
+
+}
+
+filename <- commandArgs(trailingOnly = T)[1]
+sam      <- commandArgs(trailingOnly = T)[2]
+#filename_log <- commandArgs(trailingOnly = T)[2]
+
+coal <- data.frame()
+coal <- rbind(coal, cbind(read.coal(paste0(filename,"_",sam,"_iter0.coal")), iter = paste0("Iteration 1")))
+coal <- rbind(coal, cbind(read.coal(paste0(filename,"_",sam,".coal")), iter = paste0("Iteration 1000")))
+
+coal$group1 <- as.numeric(as.factor(coal$group1))
+
+coal$haploid.coalescence.rate[which(coal$haploid.coalescence.rate == 0 & coal$epoch.start > 1e6/28)] <- coal$haploid.coalescence.rate[which(coal$haploid.coalescence.rate == 0 & coal$epoch.start > 1e6/28)-1]
+coal$epoch.start[is.infinite(coal$epoch.start)] <- 1e8
+coal %>% filter(!is.na(haploid.coalescence.rate)) %>% group_by(epoch.start, group1, group2, iter) %>% summarize(mean = mean(haploid.coalescence.rate), lower = quantile(haploid.coalescence.rate, p = 0.025), upper = quantile(haploid.coalescence.rate, p = 0.975)) %>% filter(mean > 0, !is.na(mean)) %>% droplevels() -> coal
+
+coal$group1 <- paste0("comp", coal$group1)
+
+p1 <- ggplot(coal) + geom_step(aes(x = 28*epoch.start, y = 0.5/mean, colour = group2), lwd = 1.1) +
+				#geom_stepribbon(aes(x = 28*epoch.start, ymin = 0.5/upper, ymax = 0.5/lower, fill = group2), alpha = 0.5) +
+				ggthemes::theme_few() +
+				scale_x_continuous(limit = c(5e3,1e6), trans = "log10") +
+				scale_y_continuous(trans = "log10") +
+				coord_cartesian(ylim = c(1e3,1e7)) +
+				scale_colour_manual(values = palette) +
+				scale_fill_manual(values = palette) + 
+				annotation_logticks(sides = "bl") +
+				facet_grid(iter~group1) +
+				theme(legend.position = "bottom", legend.title = element_blank()) +
+				xlab("years ago") +
+				ylab("Inverse coalescence rate") + guides(colour = guide_legend(override.aes = list(size = 3)))
+
+g <- ggplot_gtable(ggplot_build(p1))
+
+strips <- which(grepl('strip-t', g$layout$name))
+
+for (i in seq_along(strips)) {
+	  k <- which(grepl('rect', g$grobs[[strips[i]]]$grobs[[1]]$childrenOrder))
+    l <- which(grepl('titleGrob', g$grobs[[strips[i]]]$grobs[[1]]$childrenOrder))
+	  g$grobs[[strips[i]]]$grobs[[1]]$children[[k]]$gp$fill <- colours[i]
+	  #g$grobs[[strips[i]]]$grobs[[1]]$children[[l]]$children[[1]]$gp$col <- pal[i + 1]
+}
+
+strips <- which(grepl('strip-r', g$layout$name))
+
+for (i in seq_along(strips)) {
+	k <- which(grepl('rect', g$grobs[[strips[i]]]$grobs[[1]]$childrenOrder))
+	l <- which(grepl('titleGrob', g$grobs[[strips[i]]]$grobs[[1]]$childrenOrder))
+	g$grobs[[strips[i]]]$grobs[[1]]$children[[k]]$gp$fill <- "lightgrey"
+	#g$grobs[[strips[i]]]$grobs[[1]]$children[[l]]$children[[1]]$gp$col <- pal[i + 1]
+}
+
+p1 <- as.ggplot(g)
+
+
+
+########### Likeilhood
+
+foo <- as.matrix(read.table(paste0(filename, "_0.logl")))
+logl <- data.frame(iters = 1:length(foo), logl = as.numeric(as.matrix(foo)))
+
+print(head(logl))
+p2 <- ggplot(logl) + geom_point(aes(x = iters, y = logl)) + geom_line(aes(x = iters, y = logl)) + ylab("log-likelihood") + xlab("iterations") + coord_cartesian(expand = F, xlim = c(0, max(logl$iters+1)), ylim = range(logl$logl) * c(1.01,0.99)) + ggthemes::theme_few() 
+
+########### Proportions
+
+
+prop <- data.frame()
+
+foo <- read.table(paste0(filename,"_0.tau"))
+colnames(foo) <- paste0("comp", 1:ncol(foo))
+prop <- rbind(prop, cbind(iters = 1:nrow(foo), foo))
+print(head(prop))
+prop %>% pivot_longer(cols = !iters, names_to = "Components", values_to = "Proportion") -> prop
+
+p3 <- ggplot(prop) + geom_point(aes(x = iters, y = Proportion, colour = Components)) + geom_line(aes(x = iters, y = Proportion, colour = Components)) + scale_colour_manual(values = colours, name = "") + xlab("iterations") + coord_cartesian(expand = F, xlim = c(0, max(prop$iters+1)), ylim = c(0,1.0)) + ggthemes::theme_few() 
+
+
+########### Calibration curves
+
+member <- np$load(paste0(filename,"_overall_membership_",sam,".npy"))
+member2 <- np$load(paste0(filename,"_ground_truth_membership_",sam,".npy"))
+member2 <- member2[-1,]
+member  <- as.data.frame(t(member))
+member2 <- as.data.frame(t(member2))
+
+colnames(member) <- 1:ncol(member)
+colnames(member2) <- 1:ncol(member2)
+member$ID <- 1:nrow(member)
+member2$ID <- 1:nrow(member2)
+member %>% pivot_longer(col = !ID, names_to = "comp", values_to = "posterior") -> member
+member2 %>% pivot_longer(col = !ID, names_to = "comp", values_to = "truth") -> member2
+
+match <- numeric(0)
+for(i in unique(member2$comp)){
+  k <- numeric(0)
+  c <- -1
+  for(j in unique(member$comp)){
+    if( cor( subset(member, comp == j)$posterior, subset(member2, comp == i)$truth ) > c ){
+      c <- cor( subset(member, comp == j)$posterior, subset(member2, comp == i)$truth )
+      k <- j
+    }
+  }
+  match <- append(match, k)
+}
+print(match)
+member2$comp <- as.numeric(as.matrix(member2$comp))
+member2$comp <- match[member2$comp]
+df <- merge(member, member2, by = c("comp","ID"))
+df$comp <- as.matrix(df$comp)
+df$comp <- paste0("comp",df$comp)
+
+df$post_bin <- as.numeric(as.matrix(cut(df$posterior, breaks = seq(0,1,0.05), labels = seq(0,0.95,0.05)+0.025)))
+df %>% group_by(post_bin, comp) %>% summarize(truth = mean(truth)) -> df_calib 
+p4 <- ggplot(df_calib) + geom_abline(slope = 1) + geom_point(aes(x = post_bin, y = truth, colour = comp), size = 2)  + geom_line(aes(x = post_bin, y = truth, colour = comp)) + ggthemes::theme_few() + coord_cartesian(xlim = c(0,1), ylim = c(0,1), expand = F) + xlab("Mean posterior probability") + ylab("Proportion in component") + scale_colour_manual(values = colours, name = "")
+
+if(0){
+df_calib <- read.table(paste0(filename,"_calibration.txt"))
+colnames(df_calib) <- c("component", "match", "x", "y")
+df_calib$component <- paste0("comp", df_calib$component+1)
+df_calib$component <- as.factor(df_calib$component)
+df_calib %>% group_by(component) %>% filter( match == match[which.max(y)] ) -> df_calib
+p4 <- ggplot(df_calib) + geom_abline(slope = 1) + geom_point(aes(x = x, y = y, colour = component), size = 2) + geom_line(aes(x = x, y = y, colour = component)) + ggthemes::theme_few() + coord_cartesian(xlim = c(0,1), ylim = c(0,1), expand = F) + xlab("Mean posterior probability") + ylab("Proportion in component") + scale_colour_manual(values = colours, name = "")
+}
+
+
+p <- plot_grid(p2,p3,p4,ncol = 1, align = "hv", axis = "lr", labels = c("b", "c", "d"), label_size = 17)
+p <- plot_grid(p1,p, ncol = 2, rel_widths = c(1.3,1), labels = c("a", ""), label_size = 17)
+
+ggsave(p, file = paste0(filename, "_", sam,".pdf"), width = 11.5, height = 9.5)
+
