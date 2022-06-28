a101a1e5f13a89d7f7094cba3e441fcbe856855b
hrushikeshloya <loyahrushikesh@gmail.com>
2022-06-28 18:38:11 +0100

    :bug: fixes in local ancestry calcultion for sims

--- true_local_ancestry/get_local_anc.py
+++ true_local_ancestry/get_local_anc.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 from tqdm import tqdm
 import tskit
+import pdb
 
 path = sys.argv[1]
 focal = int(sys.argv[2])
@@ -18,7 +19,7 @@ def get_migrating_tracts_with_id(ts):
         N += 1
     migrations = []
     for (m, migration) in tqdm(enumerate(ts.migrations())):
-        if migration.time == migr_time:
+        if np.abs(migration.time-migr_time) < 1:
             migrations.append(
                 {
                     "left": migration.left,
@@ -33,10 +34,10 @@ def get_migrating_tracts_with_id(ts):
     migration_array = [[[]] for _ in range(N)]
     tree = ts.first()
     for (m, migration) in tqdm(enumerate(sorted_migrations)):
-        if tree.interval[0] > migration["right"]:
-            continue
-        while tree.interval[1] < migration["left"]:
-            tree.next()
+        #if tree.interval[0] > migration["right"]:
+        tree = ts.at(migration["left"])
+        #while tree.interval[1] < migration["left"]:
+        #    tree.next()
         while migration["right"] >= tree.interval[0]:
             parent_node = migration["node"]
             for i in tree.get_leaves(parent_node):
@@ -51,12 +52,16 @@ def get_migrating_tracts_with_id(ts):
 for chr in chrs:
     ts = tskit.load(path + "stdpopsim_homsap_chr" + str(chr) + ".trees")
     migrating_tracts = get_migrating_tracts_with_id(ts)
+    #import pdb; pdb.set_trace()
     for j in [focal]:  ## only first 10 are admixed
         migrating_tracts_i = migrating_tracts[j][1:]
+        #print(migrating_tracts_i[850:870])
         if migrating_tracts_i != []:
             mig = []
             startpos = migrating_tracts_i[0][0]
             for i in range(0, len(migrating_tracts_i) - 1):
+                #if migrating_tracts_i[i][1] != migrating_tracts_i[i + 1][0]:
+                    #print((i, migrating_tracts_i[i][1], migrating_tracts_i[i + 1][0]))
                 if (
                     migrating_tracts_i[i][1] != migrating_tracts_i[i + 1][0]
                     or migrating_tracts_i[i][2] != migrating_tracts_i[i + 1][2]
@@ -64,6 +69,7 @@ for chr in chrs:
                     mig.append(
                         [startpos, migrating_tracts_i[i][1], migrating_tracts_i[i][2]]
                     )
+                    #print(mig[-1])
                     startpos = migrating_tracts_i[i + 1][0]
             if len(migrating_tracts_i) > 0:
                 mig.append(
@@ -73,15 +79,14 @@ for chr in chrs:
                         migrating_tracts_i[len(migrating_tracts_i) - 1][2],
                     ]
                 )
-            # print(mig)
-            # migrating_tracts_i = np.array(migrating_tracts_i[1:])
-            # print(migrating_tracts_i)
-            # migrating_tracts_i = migrating_tracts_i[(migrating_tracts_i[:,0] != 0) | (migrating_tracts_i[:,1] != 0)]
-            if mig != []:
-                np.savetxt(
-                    path + "local_ancestry_chr" + str(chr) + "_" + str(j) + ".csv",
-                    np.array(mig),
-                    delimiter=",",
-                )
+        else:
+            mig = [[ts.first().interval[0], ts.last().interval[1], 2]]
+        if mig != []:
+            print("Saving the ground truth ancestry")
+            np.savetxt(
+                path + "local_ancestry_chr" + str(chr) + "_" + str(j) + ".csv",
+                np.array(mig),
+                delimiter=",",
+            )
 
 ## python get_local_anc.py ../example/ 52 2272 22
