f25c656f131e3c06b80b1590877b5aa8a3998dbd
hrushikeshloya <loyahrushikesh@gmail.com>
2023-06-12 11:52:04 +0100

    :turtle: adding multiple sample support, pretty plots

--- ld_curve_dating.py
+++ ld_curve_dating.py
@@ -5,7 +5,8 @@ from tqdm import tqdm
 from scipy.optimize import curve_fit
 import seaborn as sns
 import matplotlib
-import pdb
+from scipy import interpolate
+from scipy.interpolate import make_interp_spline, BSpline
 
 font = {"size": 16}
 matplotlib.rc("font", **font)
@@ -18,19 +19,28 @@ def func(dist, a, admixtimes, c):
     return a * np.exp(-admixtimes * dist) + c
 
 
-def plot_ld_curves(dist, means, admixtimes, output_prefix):
-    fig, ax = plt.subplots(len(means), len(means), figsize=(10, 10))
-    for i in range(len(means)):
-        for j in range(len(means)):
-            ax[i, j].scatter(dist, means[i, j], color="black", s=2)
-            popt, pcov = curve_fit(func, dist, means[i, j])
-            ax[i, j].plot(dist, func(dist, *popt), "--", color="gray", linewidth=1)
+def plot_ld_curves(dist, means_all, admixtimes, output_prefix):
+    num_sam = len(means_all)
+    num_clusters = len(means_all[0])
+    fig, ax = plt.subplots(num_clusters, num_clusters, figsize=(10, 10))
+    for sam in range(num_sam):
+        for i in range(num_clusters):
+            for j in range(num_clusters):
+                spl = make_interp_spline(dist, means_all[sam][i, j])
+                ax[i, j].plot(dist, spl(dist), color="gray", alpha=0.3, linewidth=0.5)
+    mean_of_all_sam = np.mean(means_all, axis=0)
+    for i in range(num_clusters):
+        for j in range(num_clusters):
+            spl = make_interp_spline(dist, mean_of_all_sam[i, j])
+            ax[i, j].plot(dist, spl(dist), color="black", alpha=1, linewidth=1)
+            popt, pcov = curve_fit(func, dist, mean_of_all_sam[i, j], maxfev=5000)
+            ax[i, j].plot(dist, func(dist, *popt), "--", color="green", linewidth=1)
     fig.text(0.5, 0.04, "Genetic distance (cM)", ha="center", va="center")
     fig.text(
         0.06, 0.5, "Relative probability", ha="center", va="center", rotation="vertical"
     )
     fig.suptitle(
-        "Co-ancestry curves (admix time = {0:.2f} generations)".format(admixtimes)
+        "Co-ancestry curves (admix time = {0:.1f} generations)".format(admixtimes)
     )
     plt.savefig(output_prefix + "_ld_curve.png")
     plt.show()
@@ -38,64 +48,96 @@ def plot_ld_curves(dist, means, admixtimes, output_prefix):
 
 def get_admixtimes(initial_values, dist, means):
     admixtimes = 0
-    for i in range(means.shape[0]):
-        popt, pcov = curve_fit(func, dist, means[i])
+    for i in range(len(means)):
+        popt, pcov = curve_fit(func, dist, means[i], maxfev=5000)
         admixtimes += popt[1]
-    admixtimes = admixtimes * 100 / means.shape[0]
+    admixtimes = admixtimes * 100 / len(means)
     return admixtimes
+
+
+def get_coancestry_per_sample(df_hap1, bin_size, bin_max, num_clusters):
+    num_bins = int(bin_max / bin_size)
+    means = np.zeros((num_bins, num_clusters, num_clusters))
+    dist = []
+    for chr in np.unique(df_hap1.chr):
+        df_chr = df_hap1[df_hap1.chr == chr]
+        grids = df_chr.genpos.values
+        post = df_chr[prob_labels].values
+        f = interpolate.interp1d(
+            df_chr.genpos.values, df_chr[prob_labels].values, axis=0
+        )
+        prob_values = f(
+            np.arange(df_chr.genpos.values[0], df_chr.genpos.values[-1], bin_size)
+        )
+        props = np.mean(prob_values, axis=0)
+        print(props)
+        for count, bin in enumerate(np.arange(0, bin_max, bin_size)):
+            ## shift prob_values by bin and multiply with itself
+            for cluster1 in range(num_clusters):
+                for cluster2 in range(num_clusters):
+                    means[count, cluster1, cluster2] += np.sum(
+                        prob_values[:, cluster1]
+                        * np.array(
+                            prob_values[count:, cluster2].tolist()
+                            + np.zeros(count).tolist()
+                        )
+                    ) / (len(prob_values) - count)
+
+        for cluster1 in range(num_clusters):
+            for cluster2 in range(num_clusters):
+                means[:, cluster1, cluster2] -= props[cluster1] * props[cluster2]
+                means[:, cluster1, cluster2] /= props[cluster1] * (1 - props[cluster1])
+                means[:, cluster1, cluster2] += 1
+
+    means = np.array(means).transpose(1, 2, 0) / len(np.unique(df_hap1.chr))
+    dist = np.arange(0, bin_max, bin_size)
+    return means, dist
 
 
 if __name__ == "__main__":
     bin_size = 0.1
-    bin_max = 50
-    num_bins = int(bin_max / bin_size)
+    bin_max = 100
     initial_values = (
         np.sqrt(np.power(10, np.random.uniform(np.log10(20), np.log10(2000))))
         # if args.t_admix_guess is None
         # else [args.t_admix_guess]
     )
     df = pd.read_csv(
-        "../real_apr23/sgdp_relate_trees/output/hazara_all_overall_membership_358_359.csv",
+        "../../Mosaic/hazara_all_overall_membership_44_45_46_47_48_49_50_51_52_53_54_55_56_57_58_59_60_61_62_63_64_65_66_67_68_69_70_71_72_73_74_75_76_77_78_79.csv",
         sep="\s+",
     )
+    num_hap = 36
     num_clusters = df.shape[1] - 3
     prob_labels = ["prob_" + str(i) for i in range(num_clusters)]
 
-    df_hap1 = df.iloc[0 : df.shape[0] // 2]
-    df_hap2 = df.iloc[df.shape[0] // 2 :]
-    for prob_col in prob_labels:
-        df_hap1[prob_col] = df_hap1[prob_col].values + df_hap2[prob_col].values
-    df_hap1 = df_hap1.sample(20000)
-    # df = pd.read_csv(
-    #     "../bedouin/output/temp_nohmm_overall_membership_0.csv",
-    #     sep="\s+",
-    # )
+    means_all = []
+    for sam in range(num_hap // 2):
+        hap_1 = 2 * sam
+        hap_2 = 2 * sam + 1
+        df_hap1 = df.iloc[
+            hap_1 * df.shape[0] // num_hap : hap_2 * df.shape[0] // num_hap
+        ]
+        df_hap2 = df.iloc[
+            hap_2 * df.shape[0] // num_hap : (hap_2 + 1) * df.shape[0] // num_hap
+        ]
+        for prob_col in prob_labels:
+            df_hap1[prob_col] = (
+                df_hap1[prob_col].values + df_hap2[prob_col].values
+            ) / 2
+        means, dist = get_coancestry_per_sample(
+            df_hap1, bin_size, bin_max, num_clusters
+        )
+        means_all.append(means)
 
-    means = np.zeros((num_bins, num_clusters, num_clusters))
-    total = np.zeros(num_bins)
-    for chr in np.unique(df_hap1.chr):
-        df_chr = df_hap1[df_hap1.chr == chr]
-        grids = df_chr.genpos.values
-        post = df_chr[prob_labels].values
-        for d1, p1 in tqdm(zip(grids, post)):
-            grids_within_range = grids[np.abs(grids - d1) < bin_max]
-            post_within_range = post[np.abs(grids - d1) < bin_max]
-            for d2, p2 in zip(grids_within_range, post_within_range):
-                means[int(np.abs(d1 - d2) // bin_size)] += np.outer(p1, p2)
-                total[int(np.abs(d1 - d2) // bin_size)] += 1
+    # for i in range(2):
+    #     for j in range(2):
+    #         means[i,j] = np.exp(-dist * 500 / 100) + np.random.normal(0, 0.05, size=dist.shape)
+    means_diag = []
+    for sam in range(num_hap // 2):
+        for i in range(num_clusters):
+            means_diag.append(means_all[sam][i, i])
 
-    means = means / total[:, None, None]
-    means = np.array(means).transpose(1, 2, 0)
-    dist = np.arange(0, bin_max, bin_size) + bin_size / 2
-    # dist = np.arange(0.01, 1, 0.001)  # vector of D genetic distances (cM) to fit
-    # means_diag = np.vstack(
-    #     (
-    #         np.exp(-dist * 500 / 100) + np.random.normal(0, 0.05, size=dist.shape),
-    #         np.exp(-dist * 550 / 100) + np.random.normal(0, 0.05, size=dist.shape),
-    #     )
-    # )
-    means_diag = np.array([means[i, i] for i in range(len(means))])
     admixtimes = get_admixtimes(initial_values, dist, means_diag)
     print("Admixtime = " + str(admixtimes))
 
-    plot_ld_curves(dist, means, admixtimes, "sim_bed")
+    plot_ld_curves(dist, means_all, admixtimes, "sim_bed")
