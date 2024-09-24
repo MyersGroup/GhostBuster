bug fix in cm grid, mut scaling with no hmm at end

calc_fixed_params.py
@@ -21,6 +21,7 @@ def fixed_parameters(
     sample_list,
     epoch_intervals_pow,
     target_branch_length_masked,
+    mutscale_masked,
     chr,
     force_build=1,
     ignore_first_epoch=False,
@@ -381,7 +382,7 @@ def fixed_parameters(
         epoch_index_in_tree = epoch_index_all[n_t]
         for c_t in range(len(denom_all[n_t])):
             denom_epochwise[n_t] += denom_all[n_t][c_t]/target_branch_length_masked[n_t][c_t]
-            denom_epochwise_unscaled[n_t] += denom_all[n_t][c_t]
+            denom_epochwise_unscaled[n_t] += denom_all[n_t][c_t]/mutscale_masked[n_t][c_t]
             
     return (
         coal_count,
@@ -392,7 +393,7 @@ def fixed_parameters(
     )
 
 
-def load_fixed_params(args, ts_list, sample, poplabels, mask_dodgy, chr_map, epoch_intervals, target_branch_length_masked, gt_ref=None, unique_groups=None, exact_pos=None):
+def load_fixed_params(args, ts_list, sample, poplabels, mask_dodgy, chr_map, epoch_intervals, target_branch_length_masked, mutscale_masked, gt_ref=None, unique_groups=None, exact_pos=None):
     chrs = list(map(int, args.chrs.split(",")))
     unique_groups = np.unique(poplabels[poplabels.INCLUDE == 1].GROUP) if unique_groups is None else unique_groups
     epoch_intervals_pow = np.power(10, epoch_intervals)
@@ -446,6 +447,7 @@ def load_fixed_params(args, ts_list, sample, poplabels, mask_dodgy, chr_map, epo
             mask_dodgy_sam_chr = mask_dodgy[np.array(chr_map) == chr]
             num_trees = np.sum(mask_dodgy_sam_chr)
             target_branch_length_masked_chr = []
+            mutscale_masked_chr = []
             # make faster #
             if chr_no == 0:
                 num_sites_per_tree = np.zeros(np.sum(mask_dodgy), dtype='int32')            
@@ -466,6 +468,7 @@ def load_fixed_params(args, ts_list, sample, poplabels, mask_dodgy, chr_map, epo
             for t in range(len(target_branch_length_masked)):
                 if chr_map_masked[t] == chr:
                     target_branch_length_masked_chr.append(target_branch_length_masked[t])
+                    mutscale_masked_chr.append(mutscale_masked[t])
             
             (coal_count, denom, denom_unscaled, proportion_of_coalescing, epoch_index) = fixed_parameters(
                 ts_list[chr_no:chr_no + 1],
@@ -477,6 +480,7 @@ def load_fixed_params(args, ts_list, sample, poplabels, mask_dodgy, chr_map, epo
                 args.sample_id,
                 epoch_intervals_pow,
                 target_branch_length_masked_chr,
+                mutscale_masked_chr,
                 chr,
                 args.force_build,
                 ignore_first_epoch=args.ignore_first_epoch,

ghost_buster.py
@@ -450,6 +450,7 @@ def random_sweep(
     mask_dodgy,
     epoch_intervals,
     target_branch_length_masked,
+    mutscale_masked,
     tree_left_bp,
     tree_right_bp,
     gen_grid_all,
@@ -470,7 +471,7 @@ def random_sweep(
             denom1_unscaled,
             proportion_of_coalescing_all1,
             epoch_index_all1,
-        ) = load_fixed_params(args, ts_list, sample, poplabels, mask_dodgy[sample_no], chr_map, epoch_intervals, target_branch_length_masked[sample_no], gt_ref, unique_groups, exact_pos)
+        ) = load_fixed_params(args, ts_list, sample, poplabels, mask_dodgy[sample_no], chr_map, epoch_intervals, target_branch_length_masked[sample_no], mutscale_masked[sample_no], gt_ref, unique_groups, exact_pos)
         num.append(num1)
         denom.append(denom1)
         denom_unscaled.append(denom1_unscaled)
@@ -680,7 +681,8 @@ def write_membership_gamma(
         np.save(f, trans_prop)
 
 def main(args):
-    if args.load_mask is not None:
+    if args.load_mask is not None or args.cm_grid is not None:
+        force_build_orig = args.force_build
         args.force_build = 1
         print("Exact positions can only be used with force_build=1")
 
@@ -772,6 +774,35 @@ def main(args):
         chr_map,
     ) = load_tree_stats(args, ts_list, poplabels, args.tree_stats_file_prefix)
 
+    ## cM grid option
+    if args.cm_grid is not None:
+        df_all = pd.DataFrame()
+        chrs = list(map(int, args.chrs.split(",")))
+        for chr_no, chr in enumerate(chrs):
+            rec_file = args.rec + str(chr) + ".txt"
+            rec_file_gz = args.rec + str(chr) + ".txt.gz"
+            if os.path.isfile(rec_file):
+                recomb_map_msprime = msprime.RateMap.read_hapmap(rec_file)
+            elif os.path.isfile(rec_file_gz):
+                recomb_map_msprime = msprime.RateMap.read_hapmap(rec_file_gz)
+            else:
+                print(f"Recombination map for chromosome {chr} not found!")
+                continue
+            df = pd.DataFrame(np.arange(tree_left_bp[0], tree_right_bp[-1], 1), columns=['pos'])
+            df['genpos'] = recomb_map_msprime.get_cumulative_mass(df['pos'].values)
+            m_grid = args.cm_grid / 100
+            df['genpos_rounded'] = (df['genpos'] / m_grid).astype('int') * m_grid
+            df = df.groupby('genpos_rounded').first().reset_index()
+            df = df.drop(columns=['genpos_rounded'])
+            df['chr'] = chr
+            df_kb = pd.DataFrame(np.arange(tree_left_bp[0], tree_right_bp[-1], force_build_orig), columns=['pos'])
+            df_kb['chr'] = chr
+            df_union = pd.concat([df[['chr', 'pos']], df_kb[['chr', 'pos']]]).drop_duplicates().sort_values(by='pos').reset_index(drop=True)
+            df_all = pd.concat([df_all, df_union], ignore_index=True)
+        print("Saving the mask file corresponding to the cM grid...")
+        df_all.to_csv(args.output + '.cm.mask', sep='\t', index=None)
+        args.load_mask = args.output + '.cm.mask'
+
     ### Filter based on recombination rates
     if args.load_mask is None:
         mask_dodgy = []
@@ -790,7 +821,7 @@ def main(args):
         exact_pos = exact_pos.sort_values(by=['chr', 'pos'])
         exact_pos = exact_pos.reset_index(drop=True)
         mask_dodgy = []
-        load_mask = load_mask_csv(args, exact_pos, tree_left_bp, tree_right_bp, chr_map)
+        load_mask = load_mask_csv(args, exact_pos, tree_left_bp, tree_right_bp, recomb_rates, chr_map)
         for sample_id in args.sample_id:
             mask_dodgy.append(load_mask)
 
@@ -865,7 +896,7 @@ def main(args):
         gt_ref = None
 
     st = time.time()
-    target_branch_length_masked = get_target_branch_length(
+    target_branch_length_masked, mutscale_masked = get_target_branch_length(
         args, poplabels, ts_list, chrs, mask_dodgy, args.sample_id, gt_ref=gt_ref, exact_pos=exact_pos
     )
     print("target branch length: " + str(time.time() - st))
@@ -893,6 +924,7 @@ def main(args):
         mask_dodgy,
         epoch_intervals,
         target_branch_length_masked,
+        mutscale_masked,
         tree_left_bp,
         tree_right_bp,
         gen_grid_all,
@@ -988,11 +1020,11 @@ def main(args):
     )
     
     print("Saving the coal. rates and proportions without using HMM")
-    target_branch_length_masked_unity = target_branch_length_masked
-    for i in range(len(target_branch_length_masked_unity)):
-        for j in range(len(target_branch_length_masked_unity[i])):
-            for k in range(len(target_branch_length_masked_unity[i][j])):
-                target_branch_length_masked_unity[i][j][k] = 1
+    # target_branch_length_masked_unity = target_branch_length_masked
+    # for i in range(len(target_branch_length_masked_unity)):
+    #     for j in range(len(target_branch_length_masked_unity[i])):
+    #         for k in range(len(target_branch_length_masked_unity[i][j])):
+    #             target_branch_length_masked_unity[i][j][k] = 1
     gen_grid_all_unscaled = copy.deepcopy(gen_grid_all)
     for sample_no in range(len(args.sample_id)):
         gen_grid_all_unscaled[sample_no] = 1e3*np.arange(0,len(gen_grid_all[sample_no]))
@@ -1011,7 +1043,7 @@ def main(args):
         n_sites,
         len(args.sample_id),
         epoch,
-        target_branch_length_masked_unity,
+        mutscale_masked,
         tree_left_bp,
         tree_right_bp,
         gen_grid_all_unscaled,
@@ -1204,31 +1236,4 @@ if __name__ == "__main__":
     np.random.seed(args.seed)  ## fix the random seed
     random.seed(args.seed)
     print(args)    
-    if args.cm_grid is not None:
-        df_all = pd.DataFrame()
-        chrs = list(map(int, args.chrs.split(",")))
-        for chr_no, chr in enumerate(chrs):
-            rec_file = args.rec + str(chr) + ".txt"
-            rec_file_gz = args.rec + str(chr) + ".txt.gz"
-            if os.path.isfile(rec_file):
-                recomb_map_msprime = msprime.RateMap.read_hapmap(rec_file)
-            elif os.path.isfile(rec_file_gz):
-                recomb_map_msprime = msprime.RateMap.read_hapmap(rec_file_gz)
-            else:
-                print(f"Recombination map for chromosome {chr} not found!")
-                continue
-            df = pd.DataFrame(np.arange(recomb_map_msprime.left[0], recomb_map_msprime.right[-1], 1), columns=['pos'])
-            df['genpos'] = recomb_map_msprime.get_cumulative_mass(df['pos'].values)
-            m_grid = args.cm_grid / 100
-            df['genpos_rounded'] = (df['genpos'] / m_grid).astype('int') * m_grid
-            df = df.groupby('genpos_rounded').first().reset_index()
-            df = df.drop(columns=['genpos_rounded'])
-            df['chr'] = chr
-            df_kb = pd.DataFrame(np.arange(recomb_map_msprime.left[0], recomb_map_msprime.right[-1], args.force_build), columns=['pos'])
-            df_kb['chr'] = chr
-            df_union = pd.concat([df[['chr', 'pos']], df_kb[['chr', 'pos']]]).drop_duplicates().sort_values(by='pos').reset_index(drop=True)
-            df_all = pd.concat([df_all, df_union], ignore_index=True)
-        print("Saving the mask file corresponding to the cM grid...")
-        df_all.to_csv(args.output + '.cm.mask', sep='\t', index=None)
-        args.load_mask = args.output + '.cm.mask'
     main(args)

utils.py
@@ -170,10 +170,20 @@ def filter_recomb_rate(
     )
     return mask_dodgy
 
-def load_mask_csv(args, membership_mask, tree_left_bp, tree_right_bp, chr_map):
+def load_mask_csv(args, membership_mask, tree_left_bp, tree_right_bp, recomb_rates, chr_map):
     mask_dodgy = np.zeros(len(tree_left_bp), dtype="bool")
+    # mask_dodgy_nan = np.zeros(len(tree_left_bp), dtype="bool")
+    recomb_rates = np.array(recomb_rates)
     count = 0
     for chr_count, chr in enumerate(np.unique(chr_map)):
+        # recomb_rates_chr = recomb_rates[chr_map == chr]
+        # for i, r in enumerate(recomb_rates_chr):
+        #     if np.isnan(r):
+        #         recomb_rates_chr[
+        #             np.abs(np.array(tree_left_bp)[chr_map == chr] - np.array(tree_left_bp)[chr_map == chr][i]) < 500000
+        #         ] = np.inf
+        # recomb_rates_chr = np.nan_to_num(recomb_rates_chr, posinf=np.nan)
+        # mask_dodgy_nan[chr_map == chr] = ~np.isnan(recomb_rates_chr)
         membership_mask_chr = membership_mask[membership_mask['chr'] == chr]
         membership_mask_chr['pos'] = membership_mask_chr['pos']
         membership_mask_chr['pos'] = membership_mask_chr['pos'].astype(int)
@@ -350,10 +360,12 @@ def get_target_branch_length(
     exact_pos is a dataframe with chr, pos where we want the target branch length exactly.
     """    
     target_branch_length = []
+    mutscale = []
     
     for sample_no, sample in enumerate(sample_list):
         count_all_tree, count_all_tree2, count_all_tree3 = 0, 0, 0
         target_branch_length_sample = List()
+        mutscale_sample = List()
         leave_one_sample_out = list(set(sample_list) - set([sample]))
         
         for chr_no, chr in enumerate(chrs):
@@ -367,7 +379,7 @@ def get_target_branch_length(
                     (
                         mut_scaling_file, hmm_file, force_build_file, start_time, end_time, ignore_first_epoch,
                         ignore_last_epoch, masking_threshold, poplabels_file,
-                        target_branch_length_sample_chr, gt_ref_na_sum, exact_pos_file
+                        target_branch_length_sample_chr, mutscale_sample_chr, gt_ref_na_sum, exact_pos_file
                     ) = pickle.load(f_pkl)
                 
                 if (
@@ -394,6 +406,11 @@ def get_target_branch_length(
                         for j in i:
                             numba_i.append(j)
                         target_branch_length_sample.append(numba_i)
+                    for i in mutscale_sample_chr:
+                        numba_i = List().empty_list(nb.types.float64)
+                        for j in i:
+                            numba_i.append(j)
+                        mutscale_sample.append(numba_i)
                     print(f"Loaded branch persistence statistics from: {branch_persistence_file_name}")
                     continue
                 else:
@@ -402,6 +419,7 @@ def get_target_branch_length(
             except:
                 print(f"Saving branch persistence statistics to: {branch_persistence_file_name}")
                 target_branch_length_sample_chr = []
+                mutscale_sample_chr = []
                 ts = ts_list[chr_no]
                 ts_edges = ts.edges()
 
@@ -480,22 +498,32 @@ def get_target_branch_length(
                             ## removing this for true trees as trees are accurate
                             if args.mut_scaling:
                                 number_window_list = scale_number_window_list(number_window_list, num_muts_list)
+                                mutscale_sample_chr.append(scale_number_window_list(np.ones_like(number_window_list), num_muts_list))
+                            else:
+                                mutscale_sample_chr.append(np.ones_like(number_window_list))
                             target_branch_length_sample_chr.append(number_window_list)
                         count_all_tree2 += 1
                     tree.next()
                                 
                 target_branch_length_sample_chr = [sublist for sublist, count in zip(target_branch_length_sample_chr, num_sites_per_tree) for _ in range(count)]
+                mutscale_sample_chr = [sublist for sublist, count in zip(mutscale_sample_chr, num_sites_per_tree) for _ in range(count)]
                 with open(branch_persistence_file_name, "wb") as f_pkl:
-                    pickle.dump([args.mut_scaling, args.hmm, args.force_build, args.start_time, args.end_time, args.ignore_first_epoch, args.ignore_last_epoch, args.masking_threshold, poplabels.values, target_branch_length_sample_chr, np.isnan(gt_ref).sum() if gt_ref is not None else None, exact_pos.values if exact_pos is not None else None], f_pkl) 
+                    pickle.dump([args.mut_scaling, args.hmm, args.force_build, args.start_time, args.end_time, args.ignore_first_epoch, args.ignore_last_epoch, args.masking_threshold, poplabels.values, target_branch_length_sample_chr, mutscale_sample_chr, np.isnan(gt_ref).sum() if gt_ref is not None else None, exact_pos.values if exact_pos is not None else None], f_pkl) 
                 for i in target_branch_length_sample_chr:
                     numba_i = List().empty_list(nb.types.float64)
                     for j in i:
                         numba_i.append(j)
                     target_branch_length_sample.append(numba_i)
+                for i in mutscale_sample_chr:
+                    numba_i = List().empty_list(nb.types.float64)
+                    for j in i:
+                        numba_i.append(j)
+                    mutscale_sample.append(numba_i)
 
         target_branch_length.append(target_branch_length_sample)
+        mutscale.append(mutscale_sample)
 
-    return target_branch_length  ## num_samples x num_trees x num_branches
+    return target_branch_length, mutscale  ## num_samples x num_trees x num_branches
 
 if __name__ == "__main__":
     load_gamma(